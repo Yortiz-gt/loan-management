60324bb1fe93895e7c7ddbd0abb7377004e9d3f6
YORTIZ-GT <yortizm1@miumg.edu.gt>
2025-11-24 20:16:04 -0600

    Initial commit

---
+++ src/main/java/com/bank/loan/management/repository/LoanRepository.java
@@ -0,0 +1,9 @@
+package com.bank.loan.management.repository;
+
+import com.bank.loan.management.model.Loan;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LoanRepository extends JpaRepository<Loan, Long> {
+}
---
+++ src/main/java/com/bank/loan/management/service/LoanService.java
@@ -0,0 +1,19 @@
+package com.bank.loan.management.service;
+
+import com.bank.loan.management.model.Loan;
+import com.bank.loan.management.repository.LoanRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class LoanService {
+
+    @Autowired
+    private LoanRepository loanRepository;
+
+    public List<Loan> getAllLoans() {
+        return loanRepository.findAll();
+    }
+}
