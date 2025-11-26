9a54cf1690c7b76d30e169d1c6fdce0a87925d60
YORTIZ-GT <yortizm1@miumg.edu.gt>
2025-11-26 13:46:44 -0600

    correccion docker compose

--- run-init-scripts.sh
+++ run-init-scripts.sh
@@ -1,7 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 # run-init-scripts.sh
 
-# Esperar a que SQL Server esté completamente listo
 echo "Esperando a que SQL Server esté listo..."
 /opt/mssql-tools/bin/sqlcmd -S db -U sa -P "$SA_PASSWORD" -Q "SELECT 1" > /dev/null 2>&1
 while [ $? -ne 0 ]
@@ -14,7 +13,6 @@ echo "SQL Server está listo."
 
 echo "Ejecutando scripts de inicialización de la base de datos..."
 
-# Ejecutar los scripts en el orden correcto
 /opt/mssql-tools/bin/sqlcmd -S db -U sa -P "$SA_PASSWORD" -i /sql-init/01-create-database.sql
 /opt/mssql-tools/bin/sqlcmd -S db -U sa -P "$SA_PASSWORD" -i /sql-init/02-create-tables.sql
 /opt/mssql-tools/bin/sqlcmd -S db -U sa -P "$SA_PASSWORD" -i /sql-init/03-insert-initial-data.sql
